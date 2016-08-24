0485a5e1ac372787e20e9849d3b84a4d1f8d2a36
Jeremy Wong <navhaxs@hotmail.com>
2016-08-25 01:07:49 +1000

    added slideshow

--- js/agency.js
+++ js/agency.js
@@ -24,3 +24,24 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+// Cycles2 slideshow progress bar
+//var progress = $('#progress'),
+//    slideshow = $( '.cycle-slideshow' );
+//
+//slideshow.on( 'cycle-initialized cycle-before', function( e, opts ) {
+//    progress.stop(true).css( 'width', 0 );
+//});
+//
+//slideshow.on( 'cycle-initialized cycle-after', function( e, opts ) {
+//    if ( ! slideshow.is('.cycle-paused') )
+//        progress.animate({ width: '100%' }, opts.timeout, 'linear' );
+//});
+//
+//slideshow.on( 'cycle-paused', function( e, opts ) {
+//   progress.stop(); 
+//});
+//
+//slideshow.on( 'cycle-resumed', function( e, opts, timeoutRemaining ) {
+//    progress.animate({ width: '100%' }, timeoutRemaining, 'linear' );
+//});
